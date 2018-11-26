630da257ca2b18e3a7163d6f743e25aa498db66f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-26 19:41:38 +0000

    Deploy yvo2/primcalq to github.com/yvo2/primcalq.git:gh-pages

---
+++ precache-manifest.3e242c1e00ce34c34130a2acaef37604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94150eedc60c47b5abeb",
+    "url": "/primcalq/static/css/main.78e8091a.chunk.css"
+  },
+  {
+    "revision": "94150eedc60c47b5abeb",
+    "url": "/primcalq/static/js/main.94150eed.chunk.js"
+  },
+  {
+    "revision": "5f7ce33a918f82104582",
+    "url": "/primcalq/static/js/1.5f7ce33a.chunk.js"
+  },
+  {
+    "revision": "6dd350c9f867d212f243",
+    "url": "/primcalq/static/js/runtime~main.6dd350c9.js"
+  },
+  {
+    "revision": "cf79f1587f3b16ed96a766c2f28199a7",
+    "url": "/primcalq/index.html"
+  }
+];
